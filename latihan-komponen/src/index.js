f9e481a47f19326c4902be521dd767fe4865bec6
IlhamArifinnn <arifinn2208@gmail.com>
2024-11-27 16:55:36 +0700

    Latihan Membuat dan Komposisi React Component

---
+++ latihan-komponen/src/index.js
@@ -0,0 +1,96 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { createRoot } from "react-dom/client";
+
+function Button({ link }) {
+  return <a href={link}>Find out More</a>;
+}
+
+function CardHeader({ image, category }) {
+  return (
+    <header>
+      <h3>{category}</h3>
+      <img src={image} alt={category} />
+    </header>
+  );
+}
+
+function CardBody({ date, title, content, link }) {
+  return (
+    <div>
+      <p>{date}</p>
+      <h2>{title}</h2>
+      <p>{content}</p>
+      <Button link={link} />
+    </div>
+  );
+}
+
+function Card({ image, category, date, title, content, link }) {
+  return (
+    <article>
+      <CardHeader image={image} category={category} />
+      <CardBody date={date} title={title} content={content} link={link} />
+    </article>
+  );
+}
+
+function Header({ title, subtitle }) {
+  return (
+    <header>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+    </header>
+  );
+}
+
+function News() {
+  // data news
+  const someNews = [
+    {
+      title: "CNN Acuire BEME",
+      date: "March 20 2022",
+      content: "CNN purchased Casey Neistat's Beme app for $25million.",
+      image: "https://picsum.photos/600/400",
+      category: "News",
+      link: "#",
+    },
+    {
+      title: "React and the WP-API",
+      date: "March 19 2022",
+      content: "The first ever decoupled starter theme for React & the WP-API.",
+      image: "https://picsum.photos/600/400",
+      category: "News",
+      link: "#",
+    },
+    {
+      title: "Nomad Lifestyle",
+      date: "March 19 2022",
+      content: "Learn our tips and tricks on living a nomadic lifestyle.",
+      image: "https://picsum.photos/600/400",
+      category: "Travel",
+      link: "#",
+    },
+  ];
+
+  return (
+    <div>
+      <Header title="Latest News" subtitle="Covering March & April 2022" />
+      {someNews.map((news) => (
+        <Card {...news} key={news.title}/>
+      ))}
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(<News />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
