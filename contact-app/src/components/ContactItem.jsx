6c4a720c409efddbd6bc0c46a9bf4cd747130d1b
IlhamArifinnn <arifinn2208@gmail.com>
2024-11-29 09:28:37 +0700

    Menambahkan Fitur Hapus Kontak

--- contact-app/src/components/ContactItem.jsx
+++ contact-app/src/components/ContactItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ContactItemImage from "./ContactItemImage";
 import ContactItemBody from "./ContactItemBody";
+import DeleteButton from "./DeleteButton";
 
-function ContactItem({ imageUrl, name, tag }) {
+function ContactItem({ imageUrl, name, tag, id, onDelete, on }) {
   return (
     <div className="contact-item">
       <ContactItemImage imageUrl={imageUrl} />
       <ContactItemBody name={name} tag={tag} />
+      <DeleteButton id={id} onDelete={onDelete} />{" "}
     </div>
   );
 }
