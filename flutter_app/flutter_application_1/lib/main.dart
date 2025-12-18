46b67d03b380ea6437a29463d234d533ec185616
IlhamArifinnn <ilha23244ti@student.nurulfikri.ac.id>
2025-12-19 05:48:41 +0700

    study: StatelessWidget dan StatefulWidget

--- flutter_app/flutter_application_1/lib/main.dart
+++ flutter_app/flutter_application_1/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -10,10 +8,55 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Hello, world!')),
-        body: Center(child: Text('Hello, ipin!')),
+      home: const Scaffold(
+        body: Center(
+          child: BiggerText(
+            text: "Hello world!",
+          ), // Ubah widget Heading ke PerubahanText
+        ),
       ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  const Heading({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+  @override
+  State<BiggerText> createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: const Text("Perbesar"),
+          onPressed: () {
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        ),
+      ],
     );
   }
 }
