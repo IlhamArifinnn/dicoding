e2b7feb69d127807b2e8ddf112d04b456101180b
IlhamArifinnn <arifinn2208@gmail.com>
2025-12-22 19:03:22 +0700

    study: listview, expanded & flexible, and navigation flutter

---
+++ flutter_app/flutter_application_1/lib/list_view/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'List View Example',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const ScrollingScreen(),
+    );
+  }
+}
+
+class ScrollingScreen extends StatelessWidget {
+  const ScrollingScreen({Key? key}) : super(key: key);
+
+  final List<int> numberList = const <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget> [
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(color: Colors.black),
+            ),
+            child: const Center(
+              child: Text('1', style: TextStyle(fontSize: 50)),
+            ),
+          ),
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(color: Colors.black),
+            ),
+            child: const Center(
+              child: Text('2', style: TextStyle(fontSize: 50)),
+            ),
+          ),
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(color: Colors.black),
+            ),
+            child: const Center(
+              child: Text('3', style: TextStyle(fontSize: 50)),
+            ),
+          ),
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(color: Colors.black),
+            ),
+            child: const Center(
+              child: Text('4', style: TextStyle(fontSize: 50)),
+            ),
+          ),
+          Container(
+            
+            
+          )
+        ],
+      ),
+    );
+  }
+}
